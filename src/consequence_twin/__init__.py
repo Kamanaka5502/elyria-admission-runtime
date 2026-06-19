76cb5b7defe68c4793d2aa907813a0ea0d569a7b
Samantha Greenwell Revita <samgreenrevita@gmail.com>
2026-06-19 11:56:18 -0600

    Update package surface naming

--- src/consequence_twin/__init__.py
+++ src/consequence_twin/__init__.py
@@ -1,4 +1,4 @@
-"""Elyria Consequence Twin."""
+"""Elyria Admission Runtime public package surface."""
 
 from .engine import AssessmentResult, Verdict, assess_movement
 from .receipt_runtime import ReceiptRecord, create_receipt, verify_receipt
